1ecae3b9330d7ea50cf48c1b9b3a5ff792d82c07
blazeaxel99 <kmoulik99@gmail.com>
2021-09-30 20:45:54 +0530

    add increase

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -9,8 +9,11 @@ class CartItem extends React.Component {
       qty: 1,
       img: "",
     };
+    this.increaseQuantity = this.increaseQuantity.bind(this);
   }
-
+  increaseQuantity = () => {
+    console.log("this", this.state);
+  };
   render() {
     const { price, title, qty } = this.state;
     return (
@@ -21,13 +24,14 @@ class CartItem extends React.Component {
         <div className="right-block">
           <div style={{ fontSize: 25 }}>{title}</div>
           <div style={{ color: "#777" }}>Rs {price}</div>
-          <div style={{ color: "#777" }}>{qty} </div>
+          <div style={{ color: "#777" }}> Qty :{qty} </div>
           <div className="cart-item-actions">
             {/*Buttons*/}
             <img
               alt="increase"
               className="action-icons"
               src="https://img-premium.flaticon.com/png/512/1665/premium/1665629.png?token=exp=1633012346~hmac=8dffab16d12bb1f05fd7d6e21c73b329"
+              onClick={this.increaseQuantity}
             />
             <img
               alt="decrease"
